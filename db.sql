43dc747c70154838cb87a2cbb76d7733b87c958d
Daniel Parrado Ortega <dparrado@linux-107-09.info2>
2017-11-08 13:33:11 +0100

    Fichero de creación de BDD actualizado, sin terminar

--- db.sql
+++ db.sql
@@ -4,6 +4,10 @@ DROP DATABASE GrumpyWorld; -- Borrar línea al terminar los tests
 CREATE DATABASE GrumpyWorld;
 USE GrumpyWorld;
 
+-- Login
+CREATE USER 'daniel'@'localhost' IDENTIFIED BY 'hola';
+GRANT SELECT, UPDATE, DELETE, EXECUTE ON GrumpyWorld.* TO 'daniel';
+
 -- Tablas
 CREATE TABLE Usuarios(
   ID INT NOT NULL AUTO_INCREMENT,
@@ -89,9 +93,8 @@ CREATE TABLE Enemigos(
   Fuerza INT NOT NULL,
   Constitucion INT NOT NULL,
   Destreza INT NOT NULL,
-  Material INT NOT NULL,
-  ProbabilidadMaterial INT NOT NULL,
   ID_Zona INT NOT NULL,
+  Jefe BIT(1) NOT NULL,
   CONSTRAINT FK_Enemigos_Zona FOREIGN KEY (ID_Zona) REFERENCES Zonas(ID) ON DELETE NO ACTION,
   PRIMARY KEY(ID)
 );
@@ -106,8 +109,42 @@ CREATE TABLE Rollos_Enemigos(
 );
 
 -- Datos iniciales
+-- Zonas
 INSERT INTO Zonas (Nombre) VALUES('Baño');
-INSERT INTO Equipables (Nombre, Tipo, Bonus, DestrezaNecesaria) VALUES ('Cartona', 'A', 1, 0);
+INSERT INTO Zonas (Nombre) VALUES('Cocina');
+INSERT INTO Zonas (Nombre) VALUES('Parque');
+
+-- Enemigos
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Stripper', 10, 20, 30, 0, ID FROM Zonas WHERE Nombre = 'Baño';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Cepillo', 10, 30, 20, 0, ID FROM Zonas WHERE Nombre = 'Baño';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Cuchilla', 30, 20, 10, 0, ID FROM Zonas WHERE Nombre = 'Baño';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Champú', 20, 10, 30, 0, ID FROM Zonas WHERE Nombre = 'Baño';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Váter', 30, 50, 20, 1, ID FROM Zonas WHERE Nombre = 'Baño';
+
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Leche', 60, 80, 70, 0, ID FROM Zonas WHERE Nombre = 'Cocina';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Zanahoria', 80, 60, 70, 0, ID FROM Zonas WHERE Nombre = 'Cocina';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Cuchara', 70, 80, 60, 0, ID FROM Zonas WHERE Nombre = 'Cocina';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Taza', 70, 60, 80, 0, ID FROM Zonas WHERE Nombre = 'Cocina';
+
+INSERT INTO Enemigos (Nombre, Fuerza, Constitucion, Destreza, Jefe, ID_Zona)
+	SELECT 'Frigorífico', 80, 100, 70, 1, ID FROM Zonas WHERE Nombre = 'Cocina';
 
 -- Pruebas
 
@@ -117,3 +154,4 @@ INSERT INTO Equipables (Nombre, Tipo, Bonus, DestrezaNecesaria) VALUES ('Cartona
 -- SELECT * FROM Usuarios;
 -- SELECT * FROM Rollos;
 -- SELECT * FROM Equipables;
+-- SELECT * FROM Enemigos
